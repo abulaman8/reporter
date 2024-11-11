08a5bfd2359907568e09ec8d69c463b193f6eecd
abulaman8 <abulaman6@gmail.com>
2024-11-11 11:44:15 +0530

    summary gen done

--- base/views.py
+++ base/views.py
@@ -1,19 +1,8 @@
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
-from .models import Admin, Student, Parent, GeneralFaculty, Coach
-from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import redirect
-from allauth.account.views import LoginView
-from allauth.account.forms import LoginForm
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.contrib import messages
-from allauth.account.views import SignupView, LoginView
-from .forms import CustomSignupForm
-import pandas as pd
 from utils.heart_rate import plot_heart_rate
 from utils.cadence_gauge import cad_plot
 from utils.cad_bpm_plot import cad_bpm_plot
-from utils.cad_bpm_rank import cad_bpm_rank
 from utils.foot_pressure import pressure_plot
 from utils.cop import cop_plot
 from utils.assym import assym_plot
@@ -24,45 +13,77 @@ from datetime import datetime
 import time
 
 
-class CustomSignupView(SignupView):
-    template_name = "account/signup.html"
-    form_class = CustomSignupForm
-
-    def form_valid(self, form):
-
-        user = form.save(self.request)
-        messages.success(self.request, "Account created successfully!")
-        return redirect("account_login")
-
-    def form_invalid(self, form):
-
-        for error in form.errors.values():
-            messages.error(self.request, error)
-        return self.render_to_response(self.get_context_data(form=form))
-
-
 def index(request):
     return render(request, "base/home.html")
 
 
-@login_required
-def base(request):
-    user = request.user
-    if Admin.objects.filter(user=user).exists():
-        return redirect("admin_dashboard")
-    elif Student.objects.filter(user=user).exists():
-        return redirect("student_dashboard")
-    elif Parent.objects.filter(user=user).exists():
-        return redirect("parent_dashboard")
-    elif GeneralFaculty.objects.filter(user=user).exists():
-        return redirect("faculty_dashboard")
-    elif Coach.objects.filter(user=user).exists():
-        return redirect("coach_dashboard")
-    else:
-        logout(request)
+def view_suumary(request):
+    data = {}
+    ssnid = request.POST.get('sessionid')
+    student_id = request.POST.get('studentid')
+    actity_time = request.POST.get('activitytime')
+    date_obj = datetime.strptime(actity_time, "%Y-%m-%dT%H:%M")
+    timestamp = int(time.mktime(date_obj.timetuple()))
+    data = {
+        "ssnid": int(ssnid),
+        "studentid": str(student_id),
+        "activitytime": timestamp
+    }
+    try:
+        # url = "http://128.199.22.46:8000/"
+        url = "http://10.21.170.133:8000/"
+
+        req = requests.get(
+            f'{url}api/reportdata/',
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(data)
+        )
+        print(req)
+        student_data_req = requests.get(
+            f"{url}api/schldetail/",
+            headers={'Content-Type': 'application/json'},
+            # data=json.dumps({"sid": "20002"})
+            data=json.dumps({"sid": str(student_id)})
+        )
+        res = req.json()
+        student_data = student_data_req.json()
+        student_data_status = student_data.get("status")
+    except Exception as e:
+        print(e)
+        messages.error(request, f"{e}")
+        return render(request, "base/home.html")
+    if len(res) == 0:
         messages.error(
-            request, "No associated role found for this user.")
-        return redirect("account_login")
+            request, f"No report found for student id {student_id} and session id {ssnid}.")
+        return render(request, "base/home.html")
+    heart_beat_metrics = res[7]["heartbeat metrics"]
+    cadence_metrics = res[6]["cadence metrics"]
+    stride_metrics = res[8]["stride metrics"]
+    stance_metrics = res[5]["stance metrics"]
+    swing_metrics = res[4]["swing metrics"]
+    cop_metrics = res[2]["cop metrics"]
+    pressure_metrics = res[0]["pressure metrics"]
+    height = student_data["height"]
+    wieght = student_data["weight"]
+    bmi = round(int(wieght) / (int(height) * int(height)), 2)
+    with open("data.json", "r") as f:
+        content = json
+    data = {
+        "name": student_data["name"] if student_data_status == "success" else "",
+        "age": student_data["age"] if student_data_status == "success" else "",
+        "height": student_data["height"] if student_data_status == "success" else "",
+        "weight": student_data["weight"] if student_data_status == "success" else "",
+        "bmi": bmi,
+        "heartrate": int(heart_beat_metrics[0]["avgheartbeat"]),
+        "cadence": int(cadence_metrics[0]["rmeancadence"]),
+        "right_toe_pressure": round(pressure_metrics[0]["ravgtoe"], 2),
+        "left_toe_pressure": round(pressure_metrics[0]["lavgtoe"], 2),
+        "right_heel_pressure": round(pressure_metrics[0]["ravgheel"], 2),
+        "left_heel_pressure": round(pressure_metrics[0]["lavgheel"], 2),
+        "stride": round(stride_metrics[0]["lmnstridelen"], 2),
+    }
+
+    return render(request, 'base/summary.html', data)
 
 
 def view_report(request):
--- reporter/urls.py
+++ reporter/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from base.views import CustomSignupView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('signup/', CustomSignupView.as_view(), name='account_signup'),
     path('', include('base.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
